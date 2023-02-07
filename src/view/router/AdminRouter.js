2df8bbb272175e612fd1742be03b065a8723b469
Trần Văn Hảo <haotvstudy@gmail.com>
2023-02-07 23:19:04 +0700

    Install adminalte

--- src/view/router/AdminRouter.js
+++ src/view/router/AdminRouter.js
@@ -1,7 +1,31 @@
+import AdminHeader from "../../components/admin/AdminHeader";
+import AdminSidebar from "../../components/admin/AdminSidebar";
+
 function AdminRouter() {
-    return (<div>
-        Admin Router
-    </div>);
+    return (
+        <>
+            <body class="hold-transition sidebar-mini layout-fixed">
+                <div class="wrapper">
+
+                    <AdminHeader />
+
+                    <AdminSidebar />
+
+
+                    <div className="content-wrapper">
+                        {/* Content Header */}
+                        <div className="content-header">
+                            <h1 className="">Dash Board</h1>
+                        </div>
+                        {/* Main content */}
+                        <section className="content">
+                            <h1>test</h1>
+                        </section>
+                    </div>
+                </div>
+            </body>
+        </>
+    );
 }
 
 export default AdminRouter;
