2f10420614177154d1c20fdcbfd6f2d38a2cc77b
Trần Văn Hảo <haotvstudy@gmail.com>
2023-02-09 22:27:00 +0700

    Check

--- src/view/public/Home.js
+++ src/view/public/Home.js
@@ -1,15 +1,14 @@
-
 function Home() {
     return (<>
         {/* Slide show */}
         <div id="myCarousel" className="carousel slide" data-bs-ride="carousel">
             <div className="carousel-indicators">
-                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" className="" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2" className="active" aria-current="true"></button>
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" className="active" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2" className="" aria-current="true"></button>
                 <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3" className=""></button>
             </div>
             <div className="carousel-inner">
-                <div className="carousel-item">
+                <div className="carousel-item ">
                     <svg className="bd-placeholder-img" width="100%" height="100vh" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"></rect></svg>
 
                     <div className="container">
