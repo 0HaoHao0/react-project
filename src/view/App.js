d12392fcc71441d2fa96da08af567fc9f7a2a38a
phuchtce150394 <phuchtce150394@fpt.edu.vn>
2023-03-03 20:45:26 +0700

    fix verify user

--- src/view/App.js
+++ src/view/App.js
@@ -4,6 +4,8 @@ import "./App.css";
 import AdminRouter from "../router/AdminRouter";
 import UserRouter from "../router/UserRouter";
 
+import Service from './user/Service';
+import ResetPassword from "./authentication/ResetPassword";
 import Login from './authentication/Login'
 import Register from './authentication/Register';
 
@@ -11,31 +13,35 @@ import PublicRouter from '../router/PublicRouter';
 import axios from 'axios';
 import { useSelector } from 'react-redux';
 import Profile from './authentication/Profile';
-import Service from './user/Service';
-import ResetPassword from "./authentication/ResetPassword";
+
 
 function App() {
   axios.defaults.baseURL = "https://localhost:44355/";
   axios.defaults.headers.common["Authorization"] =
-    localStorage.getItem("app_token");
+  localStorage.getItem("app_token");
 
   const user = useSelector((state) => state.user) || {};
 
   const { role } = user.userInfo || {};
-
+  console.log(!user.userInfo);
+  
   return (
     <Routes>
       {/* Home Router */}
       <Route path="*" element={<PublicRouter />}></Route>
+      <Route path='service' element={<Service />}></Route>
+
+      {
+        !user.userInfo && (
+          <>
+          <Route path="login" element={<Login />}></Route>
+          <Route path="register" element={<Register />}></Route>
+          <Route path="resetpassword" element={<ResetPassword />}></Route>
+          </>
+        )
+      }
       {/* Authencation Router */}
-      {!role ? (
-        <>
-          <Route path='/service' element={<Service />}></Route>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/register" element={<Register />}></Route>
-          <Route path="/resetpassword" element={<ResetPassword />}></Route>
-        </>
-      ) : (
+      {role && (
         <>
           <Route path="/profile" element={<Profile />}></Route>
         </>
