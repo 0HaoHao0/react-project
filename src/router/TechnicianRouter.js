66bf6265c4a0f4f629bf6c55588b649fd831ded1
phuchtce150394 <phuchtce150394@fpt.edu.vn>
2023-03-21 14:42:21 +0700

    Add Technician Page

--- src/router/TechnicianRouter.js
+++ src/router/TechnicianRouter.js
@@ -1,10 +1,11 @@
-import { Routes } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Technician from "../view/technician/Technician";
 
 function TechnicianRouter() {
     return (
         <div>
             <Routes>
-
+                <Route path="/" element={ <Technician/> }></Route>
             </Routes>
         </div>
     );
---
+++ src/view/technician/Technician.js
@@ -0,0 +1,16 @@
+
+
+
+
+function Technician() {
+
+    return ( 
+        <>
+            <div className="technician">
+                <h1>Technician Page</h1>
+            </div>
+        </> 
+    );
+}
+
+export default Technician;
