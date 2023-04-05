4c6e6d58ac561b48d21fc3e9c552efcde747b188
Trần Văn Hảo <haotvstudy@gmail.com>
2023-04-05 16:22:51 +0700

    Update Profile

--- src/view/authentication/Profile.js
+++ src/view/authentication/Profile.js
@@ -1,25 +1,31 @@
 import React, { useEffect } from "react";
-// import { useDispatch, useSelector } from "react-redux";
 import { useState } from "react";
 import { toast } from "react-toastify";
 import './Profile.scss'
 import moment from "moment";
+import sampleFile from '../../assets/file/MedicalRecordsReleaseForm.docx'
 
 // api
 import { updateAvatar } from "../../services/admin/user/apiUser";
 import { getUserInfo } from "../../services/authorization/apILogin";
+import { getAllAppointment, getDoctorById, getPatientById, updateCertificate, updateMedicalRecord, updatePassword, updateProfile } from "../../services/authorization/apIProfile";
 
 // Tab Menu
 import { TabMenu } from 'primereact/tabmenu';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import { Tag } from 'primereact/tag';
+
 import Swal from "sweetalert2";
 import { useRef } from "react";
-import { updateProfile } from "../../services/authorization/apIProfile";
 import { useDispatch } from "react-redux";
 import { createUser } from "../../redux/features/userSlide";
+import { Link } from "react-router-dom";
 
 
 function Profile() {
   const render = useRef(false)
+
   const [userInfo, setUserInfo] = useState()
 
 
@@ -33,13 +39,12 @@ function Profile() {
       } else if (res.status < 500) {
         toast.error(res.data);
       } else {
-        toast.error("Server is busy !!!");
+        toast.error("Something was wrong, please contact to admin !!!");
       }
     };
 
     if (render.current === true) {
       getData();
-      console.log('check');
     }
 
     return () => {
@@ -48,16 +53,15 @@ function Profile() {
   }, [])
 
   return (<>
-    <div className="profile my-5 pt-5">
-      <div className="container-fluid ">
-        <div className=" border shadow">
+    <div className="profile my-5 pt-5 ">
+      <div className="container-fluid" >
+        <div className=" border shadow-sm">
           {userInfo ?
             <>
               {/* Header */}
               <div className="profile-header p-2">
                 <ProfileHeader userInfo={userInfo} setUserInfo={setUserInfo}></ProfileHeader>
               </div>
-              <hr className="mx-5" />
               {/* Body */}
               <div className="profile-body p-2">
                 <ProfileBody userInfo={userInfo} setUserInfo={setUserInfo}></ProfileBody>
@@ -213,6 +217,7 @@ function ProfileHeader(props) {
         ...preveState,
         [name]: value
       }))
+      console.log(value);
     }, 1000);
 
   }
@@ -357,8 +362,11 @@ function ProfileHeader(props) {
 //Profile Body
 
 function ChangePassword(props) {
-  const [userData, setUserData] = useState();
+  const { userInfo } = props;
+
+  const [password, setPassword] = useState();
   const [validate, setValidate] = useState();
+
   const [showOldPassword, setShowOldPassword] = useState(false);
   const [showNewPassword, setShowNewPassword] = useState(false);
   const [showNewConfirmPassword, setShowNewConfirmPassword] = useState(false);
@@ -366,11 +374,55 @@ function ChangePassword(props) {
   const handleUpdatePassword = (e) => {
     e.preventDefault();
     // handle update password logic here
+    Swal.fire({
+      title: 'Are you sure?',
+      text: 'This item will be update.',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, update it!',
+      cancelButtonText: 'Cancel',
+      allowOutsideClick: false,
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        // Code to update the item goes here
+        Swal.fire({
+          title: "Loading...",
+          html: "Please wait a moment",
+        });
+        Swal.showLoading();
+
+
+        const res = await updatePassword(password, userInfo);
+
+
+        Swal.close();
+
+        if (res.status === 200) {
+
+          toast.success('Update successful')
+        } else if (res.status < 500) {
+          Swal.fire({
+            icon: "error",
+            html: res.data[0].description,
+          });
+        } else {
+          Swal.fire({
+            icon: "error",
+            title: "Something wrong!",
+          });
+        }
+
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        toast.error('Action Cancelled')
+      }
+    });
+
+
   }
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setUserData((prevState) => ({
+    setPassword((prevState) => ({
       ...prevState,
       [name]: value
     }));
@@ -381,11 +433,11 @@ function ChangePassword(props) {
     setValidate((preveState) => ({
       ...preveState,
       [name]: {
-        validState: value?.length > 6 ? 'valid' : 'invalid',
-        description: value?.length > 6 ? null : 'Password must be at least 6 characters long'
+        validState: value?.length >= 6 ? 'valid' : 'invalid',
+        description: value?.length >= 6 ? null : 'Password must be at least 6 characters long'
       }
     }))
-    if (name === 'newConfirmPassword' && value !== userData.newPassword) {
+    if (name === 'newConfirmPassword' && value !== password.newPassword) {
       setValidate((preveState) => ({
         ...preveState,
         [name]: {
@@ -448,28 +500,386 @@ function ChangePassword(props) {
 
 
 
-function MedicalRecord(params) {
+function MedicalRecord(props) {
+  const load = useRef(false);
+  const { userInfo } = props;
+
+  const [userData, setUserData] = useState();
+
+  const [isUpdate, setIsUpdate] = useState(false)
+
+  useEffect(() => {
+    const loadData = async () => {
+      Swal.fire({
+        title: "Loading...",
+        html: "Please wait a moment",
+      });
+      Swal.showLoading();
+
+      const res = await getPatientById(userInfo.id);
+
+      Swal.close()
+      if (res.status === 200) {
+        setUserData(res.data)
+      }
+      else if (res.status < 500) {
+        toast.error(res.data);
+      } else {
+        toast.error("Something was wrong, please contact to admin !!!");
+      }
+
+    }
+    if (load.current === true) {
+      loadData();
+    }
+    return () => {
+      load.current = true
+    }
+  }, [userInfo.id])
+
+
+  const handleUpdateMedicalRecord = () => {
+    // handle update logic here
+    Swal.fire({
+      title: 'Are you sure?',
+      text: 'This item will be update.',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, update it!',
+      cancelButtonText: 'Cancel',
+      allowOutsideClick: false,
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        // Code to update the item goes here
+        Swal.fire({
+          title: "Loading...",
+          html: "Please wait a moment",
+        });
+        Swal.showLoading();
+        let formData = new FormData();
+
+        formData.append("id", userData.id);
+        formData.append("file", userData.file);
+
+        const res = await updateMedicalRecord(formData);
+
+
+        Swal.close();
+
+        if (res.status === 200) {
+          setIsUpdate(false)
+          setUserData(res.data)
+
+          toast.success('Update successful')
+        } else if (res.status < 500) {
+          Swal.fire({
+            icon: "error",
+            html: res.data[0].description,
+          });
+        } else {
+          Swal.fire({
+            icon: "error",
+            title: "Something wrong!",
+          });
+        }
+
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        toast.error('Action Cancelled')
+      }
+    });
+  }
+
   return (
     <>
       <div className="alert alert-secondary" role="alert">
         Medical Record
       </div>
+      <div className="d-flex flex-column align-items-center p-5">
+
+        {userData && <>
+          <div className="form-group w-50">
+            <div className="d-flex align-items-center ">
+              <label htmlFor="file" className="">File: </label>
+              {
+                !isUpdate ?
+                  <>
+                    <a href={userData.medicalRecordFile.fileURL} target="_blank" className="mx-2 btn btn-success btn-sm" rel="noreferrer" >View</a>
+                    <button className="btn btn-primary btn-sm" onClick={() => { setIsUpdate(isUpdate ? false : true) }}>Update</button>
+
+                  </>
+                  :
+                  <>
+                    <div>
+                      <a href={sampleFile} download={true} className="mx-2 ">Sample File</a>
+                    </div>
+                  </>
+              }
+            </div>
+            {isUpdate &&
+              <>
+                <input id="file" className="form-control my-2" type="file" name="file" accept=".docx"
+                  onChange={(e) => {
+                    setUserData((preveState) => ({
+                      ...preveState,
+                      'file': e.target.files[0],
+                    }))
+                  }} />
+                <div className="btn-group" role="group" aria-label="Button group">
+                  <button className="btn btn-primary" onClick={handleUpdateMedicalRecord}>Update</button>
+                  <button className="btn btn-danger" onClick={() => { setIsUpdate(isUpdate ? false : true) }}>Cancel</button>
+                </div>
+              </>
+            }
+          </div>
+          <div className="form-group w-50">
+            <label htmlFor="timeCreated">Time Created: </label>
+            <input id="timeCreated" className="form-control" type="text" name="timeCreated" placeholder={userData.medicalRecordFile.timeCreated.split("T")[0]} />
+          </div>
+          <div className="form-group w-50">
+            <label htmlFor="lastTimeModified">Last Time Modified: </label>
+            <input id="lastTimeModified" className="form-control" type="text" name="lastTimeModified" placeholder={userData.medicalRecordFile.lastTimeModified.split("T")[0]} />
+          </div>
+        </>
+        }
+      </div>
+
     </>
   );
 }
 
-function AppointmentRate(params) {
+function Certificate(props) {
+  const load = useRef(false);
+  const { userInfo } = props;
+
+  const [userData, setUserData] = useState();
+
+  const [isUpdate, setIsUpdate] = useState(false)
+
+  useEffect(() => {
+    const loadData = async () => {
+      Swal.fire({
+        title: "Loading...",
+        html: "Please wait a moment",
+      });
+      Swal.showLoading();
+
+      const res = await getDoctorById(userInfo.id);
+
+      Swal.close()
+      if (res.status === 200) {
+        setUserData(res.data)
+      }
+      else if (res.status < 500) {
+        toast.error(res.data);
+      } else {
+        toast.error("Something was wrong, please contact to admin !!!");
+      }
+
+    }
+    if (load.current === true) {
+      loadData();
+    }
+    return () => {
+      load.current = true
+    }
+  }, [userInfo.id])
+
+
+  const handleUpdateCertificate = () => {
+    // handle update logic here
+    Swal.fire({
+      title: 'Are you sure?',
+      text: 'This item will be update.',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, update it!',
+      cancelButtonText: 'Cancel',
+      allowOutsideClick: false,
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        // Code to update the item goes here
+        Swal.fire({
+          title: "Loading...",
+          html: "Please wait a moment",
+        });
+        Swal.showLoading();
+        let formData = new FormData();
+
+        formData.append("Id", userData.id);
+        formData.append("CertificateFile", userData.file);
+
+        const res = await updateCertificate(formData);
+
+
+        Swal.close();
+
+        if (res.status === 200) {
+          setIsUpdate(false)
+          setUserData(res.data)
+
+          toast.success('Update successful')
+        } else if (res.status < 500) {
+          Swal.fire({
+            icon: "error",
+            html: res.data[0].description,
+          });
+        } else {
+          Swal.fire({
+            icon: "error",
+            title: "Something wrong!",
+          });
+        }
+
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        toast.error('Action Cancelled')
+      }
+    });
+  }
+
   return (
     <>
       <div className="alert alert-secondary" role="alert">
-        Appointment Rate
+        Certificate
       </div>
+      <div className="d-flex flex-column align-items-center p-5">
+
+        {userData && <>
+          <div className="form-group w-50">
+            <div className="d-flex align-items-center ">
+              <label htmlFor="file" className="">File: </label>
+              {
+                !isUpdate &&
+                <>
+                  <a href={userData.certificate.fileURL} target="_blank" className="mx-2 btn btn-success btn-sm" rel="noreferrer" >View</a>
+                  <button className="btn btn-primary btn-sm" onClick={() => { setIsUpdate(isUpdate ? false : true) }}>Update</button>
+
+                </>
+
+              }
+            </div>
+            {isUpdate &&
+              <>
+                <input id="file" className="form-control my-2" type="file" name="file" accept=".pdf"
+                  onChange={(e) => {
+                    setUserData((preveState) => ({
+                      ...preveState,
+                      'file': e.target.files[0],
+                    }))
+                  }} />
+                <div className="btn-group" role="group" aria-label="Button group">
+                  <button className="btn btn-primary" onClick={handleUpdateCertificate}>Update</button>
+                  <button className="btn btn-danger" onClick={() => { setIsUpdate(isUpdate ? false : true) }}>Cancel</button>
+                </div>
+              </>
+            }
+          </div>
+          <div className="form-group w-50">
+            <label htmlFor="timeCreated">Time Created: </label>
+            <input id="timeCreated" className="form-control" type="text" name="timeCreated" placeholder={userData.certificate?.timeCreated.split("T")[0]} />
+          </div>
+          <div className="form-group w-50">
+            <label htmlFor="lastTimeModified">Last Time Modified: </label>
+            <input id="lastTimeModified" className="form-control" type="text" name="lastTimeModified" placeholder={!userData.certificate ? userData.certificate.lastTimeModified.split("T")[0] : null} />
+          </div>
+        </>
+        }
+      </div>
+
+    </>
+  );
+}
+
+function AppointmentHistory(props) {
+  const load = useRef(false)
+  const { userInfo } = props
+
+  const [appointments, setAppointments] = useState()
+  const [filter, setFilter] = useState({
+    id: userInfo.id
+  })
+
+  const getSeverity = (status) => {
+    switch (status) {
+      case 'Complete':
+        return 'danger';
+
+      case 'NotYet':
+        return 'success';
+
+      case 'Accept':
+        return 'info';
+
+      case 'Cancel':
+        return 'warning';
+      default:
+        return 'default';
+    }
+  };
+
+  useEffect(() => {
+
+    const loadAppointment = async () => {
+      Swal.fire({
+        title: "Loading...",
+        html: "Please wait a moment",
+      });
+      Swal.showLoading();
+
+      const res = await getAllAppointment(filter);
+
+      Swal.close()
+      if (res.status === 200) {
+        setAppointments(res.data)
+      }
+      else if (res.status < 500) {
+        toast.error(res.data);
+      } else {
+        toast.error("Something was wrong, please contact to admin !!!");
+      }
+    }
+
+    if (load.current === true) {
+      loadAppointment()
+    }
+
+    return () => {
+      load.current = true;
+    }
+  }, [filter])
+
+  const dateBodyTemplate = (rowData) => {
+    return rowData.date.split("T")[0];
+  };
+  const statusBodyTemplate = (rowData) => {
+    return <Tag value={rowData.state} severity={getSeverity(rowData.state)} />;
+  };
+
+  return (
+    <>
+      <div className="alert alert-secondary" role="alert">
+        Appointment History
+      </div>
+      {appointments ?
+        <DataTable value={appointments.data} selectionMode="single" paginator rows={10} rowsPerPageOptions={[10, 25]} tableStyle={{ minWidth: '50rem' }}
+          paginatorTemplate="RowsPerPageDropdown FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink"
+          currentPageReportTemplate="{first} to {last} of {totalRecords}">
+          <Column field="service.serviceName" header="Service Name" style={{ width: '25%' }}></Column>
+          <Column field="doctor.baseUser.fullName" header="Doctor" style={{ width: '25%' }}></Column>
+          <Column field="state" header="State" body={statusBodyTemplate} style={{ width: '25%' }}></Column>
+          <Column field="date" header="Date" body={dateBodyTemplate} style={{ width: '25%' }}></Column>
+          <Column field="time" header="Time" style={{ width: '25%' }}></Column>
+
+        </DataTable>
+        :
+        null
+      }
+
     </>
   );
 }
 
 function ProfileBody(props) {
-  const { userInfo, setUserInfo } = props;
+  const { userInfo } = props;
 
   const [activeIndex, setActiveIndex] = useState({ index: 0, label: "Change Password" })
 
@@ -481,7 +891,7 @@ function ProfileBody(props) {
 
     if (userInfo && userInfo.role === 'Patient') {
       items.push({ label: 'Medical Record', icon: 'pi pi-fw pi-file' });
-      items.push({ label: 'Appointment Rate', icon: 'pi pi-fw pi-star' });
+      items.push({ label: 'Appointment History', icon: 'pi pi-fw pi-star' });
     }
     else if (userInfo && userInfo.role === 'Doctor') {
       items.push({ label: 'Certificate', icon: 'pi pi-fw pi-file' });
@@ -509,15 +919,20 @@ function ProfileBody(props) {
           }))} />
         </div>
 
-        {activeIndex.label === "Change Password" && <ChangePassword userInfo={userInfo} setUserInfo={setUserInfo}></ChangePassword>}
+        {activeIndex.label === "Change Password" && <ChangePassword userInfo={userInfo} ></ChangePassword>}
 
-        {activeIndex.label === "Medical Record" && <MedicalRecord></MedicalRecord>}
+        {activeIndex.label === "Medical Record" && <MedicalRecord userInfo={userInfo}></MedicalRecord>}
 
-        {activeIndex.label === "Appointment Rate" && <AppointmentRate></AppointmentRate>}
+        {activeIndex.label === "Appointment History" && <AppointmentHistory userInfo={userInfo}></AppointmentHistory>}
 
-        {activeIndex.label === "Certificate" && null}
+        {activeIndex.label === "Certificate" && <Certificate userInfo={userInfo}></Certificate>}
 
-        {activeIndex.label === "Email Validate" && null}
+        {activeIndex.label === "Email Validate" &&
+          <div className="w-100 text-center mt-5">
+            <div> <Link to={'/email-confirm'} className="btn btn-primary">Validate</Link></div>
+          </div>
+        }
+
 
       </div>
     </>
